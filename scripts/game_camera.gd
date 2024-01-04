80e6cc8003089a03e341623267ca5f1061985d7d
velcronator <valacos7@gmail.com>
2024-01-05 01:11:55 +1100

    Overlapping platforms

--- scripts/game_camera.gd
+++ scripts/game_camera.gd
@@ -1,7 +1,11 @@
 extends Camera2D
 
+@onready var destroyer = $Destroyer
+@onready var destroyer_shape = $Destroyer/CollisionShape2D
+
 @export var limit_distance = 420
 
+var depth_of_platform_catcher = 200
 var viewport_size
 
 var player: Player = null
@@ -14,11 +18,24 @@ func _ready() -> void:
 	# not necessary.
 	limit_left = 0
 	limit_right = viewport_size.x
+	
+	destroyer.position.y = viewport_size.y
+	
+	var rect_shape = RectangleShape2D.new()
+	var rect_shape_size = Vector2(viewport_size.x, depth_of_platform_catcher)
+	rect_shape.set_size(rect_shape_size)
+	destroyer_shape.shape = rect_shape
 
 func _process(_delta: float) -> void:
 	if player:
 		if limit_bottom > player.global_position.y + limit_distance:
 			limit_bottom = player.global_position.y + limit_distance
+			
+	var overlapping_areas = destroyer.get_overlapping_areas()
+	if overlapping_areas.size() > 0:
+		for area in overlapping_areas:
+			if area is Platform:
+				area.queue_free()
 
 func setup_camera(_player: Player):
 	if _player != null:
