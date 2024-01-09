83e68a8d74f546d7d1c0c105814e5b72b19abf8c
velcronator <valacos7@gmail.com>
2024-01-10 10:35:13 +1100

    Disabling buttons

--- scripts/base_screen.gd
+++ scripts/base_screen.gd
@@ -5,6 +5,8 @@ var tween_time = 0.5
 func _ready() -> void:
 	visible = false
 	modulate.a = 0.0
+	# disable all buttons
+	get_tree().call_group("buttons", "set_disabled", true)
 
 func appear():
 	visible = true
@@ -14,6 +16,7 @@ func appear():
 
 
 func  disappear():
+	get_tree().call_group("buttons", "set_disabled", true)
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "modulate:a", 0.0 , tween_time)
 	return tween
